b8a041e1f706497e90c9b94de918150d4ab505c4
julienPAU <juliendiablo@aol.com>
2024-09-02 18:49:22 +0200

    essai de fetch les données et de les afficher. il reste à la passer aux différents composants

--- app/ui/card/Card.tsx
+++ app/ui/card/Card.tsx
@@ -6,12 +6,30 @@ import Button from './Button';
 import Img from './Img';
 import Refuge from './Refuge';
 import Title from './Title';
-export default function Card() {
+
+export default async function Card() {
+  /* fecth la liste des animaux depuis http://localhost:3000/api/ */
+  const data = await fetch('http://localhost:3000/api/');
+  // const animals = await data.json();
+  // console.log(animals);
+
   return (
     <>
       <div className="flex flex-wrap justify-center gap-10 mt-8 mb-8">
+        {/* {animals.map((animal) => (
+          <div
+            key={animal.id}
+            className="w-10/12 sm:w-1/2 md:w-1/3 lg:w-1/6 bg-card-bg rounded-lg overflow-hidden shadow-md hover:shadow-lg hover:-translate-y-1 transition-transform duration-300 ease-in-out text-lg"
+          >
+            <p>{animal.name}</p>
+            <p>{animal.breed}</p>
+            <p>{animal.age}</p>
+            <p>{animal.location}</p>
+          </div>
+        ))} */}
         <div className="w-10/12 sm:w-1/2 md:w-1/3 lg:w-1/6 bg-card-bg rounded-lg overflow-hidden shadow-md hover:shadow-lg hover:-translate-y-1 transition-transform duration-300 ease-in-out text-lg">
           <Img />
+
           <div className="p-4">
             <Title />
             <Refuge />
