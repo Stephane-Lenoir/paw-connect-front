6ad7c810443f6985dc18b78f6c64ea49d2036820
julienPAU <juliendiablo@aol.com>
2024-09-03 15:56:04 +0200

    maj des liens homepage et adopter

--- app/ui/header/Logo.tsx
+++ app/ui/header/Logo.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Logo() {
   return (
     <>
-      <Link href={'#'} className="text-xl ">
+      <Link href="/" className="text-xl ">
         <Image src="/logo.png" alt="logo" width={200} height={200}></Image>
       </Link>
     </>
