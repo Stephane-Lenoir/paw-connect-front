92e2ddc27789445a4fcce421272e2b2640da0e36
julienPAU <juliendiablo@aol.com>
2024-09-09 17:39:03 +0200

    card modification

--- app/ui/animals/Edit.tsx
+++ app/ui/animals/Edit.tsx
@@ -38,10 +38,6 @@ export function EditAnimal() {
     fetchData();
   }, [userId]);
 
-  console.log(userId);
-
-  console.log(animals);
-
   return (
     <>
       <h3 className="font-bold text-2xl mb-6 mt-6 text-center">Vos animaux</h3>
@@ -51,14 +47,40 @@ export function EditAnimal() {
             key={animal.id}
             className="w-80 bg-card-bg rounded-lg overflow-hidden shadow-md hover:shadow-lg hover:-translate-y-1 transition-transform duration-300 ease-in-out text-lg"
           >
-            {/* <Img photo={animal.photo} /> */}
-
-            <Image src={animal.photo} alt="photo" width={300} height={300} />
+            <div className="w-full h-80 relative overflow-hidden rounded-lg">
+              <Image
+                src={animal.photo}
+                alt="Animal"
+                layout="fill"
+                objectFit="cover"
+                className="bg-primary-color"
+              />
+            </div>
 
             <div className="p-4">
-              {/* <Title name={animal.name} gender={animal.gender} />
-              <Refuge refuge={animal.user.name} /> */}
-              <p> {animal.name}</p>
+              <p>Nom : {animal.name}</p>
+              <p>Description : {animal.description}</p>
+              <p>Disponibilité : {animal.availability}</p>
+              <p>Genre : {animal.gender}</p>
+              <p>Race : {animal.race}</p>
+              <p>Location : {animal.location}</p>
+              <p>Espéce : {animal.species}</p>
+
+              <div className="flex">
+                <button
+                  type="button"
+                  className="bg-secondary-color text-white px-4 py-2 rounded-full mt-4 hover:bg-primary-color transition-colors duration-300 ease-in-out w-1/3 block mx-auto text-base font-caveat"
+                >
+                  Modifier
+                </button>
+
+                <button
+                  type="button"
+                  className="bg-secondary-color text-white px-4 py-2 rounded-full mt-4 hover:bg-primary-color transition-colors duration-300 ease-in-out w-1/3 block mx-auto text-base font-caveat"
+                >
+                  Supprimer
+                </button>
+              </div>
             </div>
           </div>
         ))}
