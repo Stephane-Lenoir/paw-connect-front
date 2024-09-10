987b4883bafcb486b1375a565260f2265fde3150
Stephane Lenoir <fanster@laposte.net>
2024-09-10 15:17:19 +0200

    detai lall Assos

--- app/ui/location/TitleAssociation.tsx
+++ app/ui/location/TitleAssociation.tsx
@@ -1,7 +1,9 @@
-export default function TitleAssociation() {
+interface TitleAssociationProps {
+  title: string;  // Déclare un type pour les props
+}
+
+export default function TitleAssociation({ title }: TitleAssociationProps) {
   return (
-    <>
-      <p className="font-bold">Le refuge pour Animaux APAK</p>
-    </>
+    <p className="font-bold">{title}</p>  // Utilise la prop `title` pour afficher le titre de l'association
   );
 }
