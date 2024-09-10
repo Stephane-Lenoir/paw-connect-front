385db0cb7656f3788c9a3712cf0eea7728fb05e6
Fabien Bourdais <fabienbourdais94@gmail.com>
2024-09-10 09:11:29 +0200

    save for change branch

--- app/ui/accomodationForm/AccomodationForm.tsx
+++ app/ui/accomodationForm/AccomodationForm.tsx
@@ -1,10 +1,37 @@
 'use client';
 
 export default function AdoptionForm() {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+    try {
+      const response = await fetch('/requests', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        console.log('Données envoyées avec succès');
+        // Réinitialiser le formulaire après la soumission réussie
+        event.target.reset();
+      } else {
+        console.error("Une erreur est survenue lors de l'envoi des données");
+      }
+    } catch (error) {
+      console.error("Une erreur est survenue lors de l'envoi des données :", error);
+    }
+  };
+
   return (
     <>
       <h2 className="text-center text-2xl p-2 font-bold">Formulaire d'hébergement</h2>
-      <form className="m-2 flex flex-col gap-px items-center">
+      <form className="m-2 flex flex-col gap-px items-center" onSubmit={handleSubmit}>
         <div className=" text-xl">
           <h3 className="p-2">Nom :</h3>
           <input type="text" placeholder="Nom" className="input input-bordered w-full max-w-xs" />
