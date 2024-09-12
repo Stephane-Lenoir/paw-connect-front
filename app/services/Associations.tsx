6c8bd86360d16f17b7629560a6ec69f7e252a766
Matthieu Rault <rault.matthieu@hotmail.fr>
2024-09-12 10:26:08 +0200

    feat: update modal for association detail and refacto

--- app/services/Associations.tsx
+++ app/services/Associations.tsx
@@ -4,7 +4,6 @@ import axios from './axiosConfig';
 export async function getAllAssociations() {
   try {
     const response = await axios.get('associations');
-    console.log('Response:', response.data);
     return response.data;
   } catch (error) {
     console.error(error);
@@ -15,10 +14,9 @@ export async function getAllAssociations() {
 export const getOneAssociation = async (id: string) => {
   try {
     const response = await axios.get(`associations/${id}`);
-    console.log('Response:', response.data);
     return response.data;
   } catch (error) {
-    console.error("Error fetching association:", error);
+    console.error('Error fetching association:', error);
     throw error;
   }
 };
