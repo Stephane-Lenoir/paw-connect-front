f2c4d2ae057f669f13e4c8a98877e534ff3d9672
Matthieu Rault <rault.matthieu@hotmail.fr>
2024-09-12 14:58:09 +0200

    fix: link with dashboard and accomodation

--- app/ui/loginModal/Modal.tsx
+++ app/ui/loginModal/Modal.tsx
@@ -69,7 +69,7 @@ export default function Modal() {
                 >
                   <li>
                     <Link
-                      href="dashboard"
+                      href="/dashboard"
                       className="block px-4 py-2 text-stone-950 text-lg hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                     >
                       Votre compte
